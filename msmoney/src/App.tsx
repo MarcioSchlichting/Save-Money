4b522b84228991bdb856332f1cba8e1024a5626c
Marciosergios <marciosergios2020@gmail.com>
2022-01-05 08:34:09 -0300

    Finishing header

---
+++ msmoney/src/App.tsx
@@ -0,0 +1,11 @@
+import { Header } from './components/Header/index';
+import { GlobalStyle } from './styles/global';
+
+export default function App(){
+    return (
+        <>
+            <Header/>
+            <GlobalStyle/>
+        </>
+    )
+}
