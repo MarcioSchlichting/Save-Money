c5a2904b76d0485b7de683edf92459d5f0b5ab76
Marciosergios <marciosergios2020@gmail.com>
2022-01-06 09:01:42 -0300

    customizing new transaction modal

---
+++ msmoney/src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,35 @@
+import Modal from 'react-modal'
+import { Container } from './styles'
+import closeImage from '../../assets/close.svg';
+
+
+interface NewTransactionModalProps {
+    isOpen: boolean;
+    onRequestClose: () => void;
+}
+
+export function NewTransactionModal( { isOpen, onRequestClose} : NewTransactionModalProps){
+    return (
+        <Modal
+            isOpen={ isOpen }
+            onRequestClose={ onRequestClose }
+            overlayClassName="react-modal-overlay"
+            className="react-modal-content"
+        >
+            <Container>
+                <button 
+                    type="button" 
+                    onClick={ onRequestClose } 
+                    className="react-modal-close">
+                    <img src={ closeImage } alt="Fechar modal"/>
+                </button>
+                <h2>Registrar transação</h2>
+                <input placeholder="Título"/>
+                <input type="number" placeholder="Valor"/>
+                <input placeholder="Categoria"/>
+                <button type="submit">Cadastrar</button>
+            </Container>
+
+        </Modal>
+    )
+}
